310016cca128d68f5d0e587cc5bffb32022c97b8
Abhishek Pradhan <contact.abhishekpradhan@gmail.com>
2024-12-12 19:42:16 +0530

    adding route

--- main.go
+++ main.go
@@ -1,9 +1,27 @@
 package main
 
-import(
-	"fmt"
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
 
-func main () {
+func main(
+	app := fiber.New()
+
+	app.Get("/", func (c *fiber.Ctx) error {
+        return c.SendString("Welcome to calculator")
+    })
+
+
+	app.Get("/calculate", unc(c *fiber.Ctx) error {
+		num1Str := c.Query("num1")
+		num2Str := c.Query("num2")
+		operator := c.Query("operator")
+	})
+
 	
-}
+
+
+    log.Fatal(app.Listen(":3000"))
+)
