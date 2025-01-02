90134333b5dc560b9e7b5837f05dfb98ab9f7e6e
Abhishek Pradhan <contact.abhishekpradhan@gmail.com>
2025-01-02 11:47:41 +0530

    update: add,sub,mul,div

--- main.go
+++ main.go
@@ -2,26 +2,115 @@ package main
 
 import (
 	"log"
+	"strconv"
 
 	"github.com/gofiber/fiber/v2"
 )
 
-func main(
+func main() {
+
 	app := fiber.New()
 
-	app.Get("/", func (c *fiber.Ctx) error {
-        return c.SendString("Welcome to calculator")
-    })
-
-
-	app.Get("/calculate", unc(c *fiber.Ctx) error {
-		num1Str := c.Query("num1")
-		num2Str := c.Query("num2")
-		operator := c.Query("operator")
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("calculator-home")
 	})
 
-	
+	app.Get("/addition", func(c *fiber.Ctx) error {
+		num1Str := c.Query("x")
+		num2Str := c.Query("y")
 
+		if num1Str == "" || num2Str == "" {
+			return c.Status(400).SendString("Please provide x, y")
+		}
 
-    log.Fatal(app.Listen(":3000"))
-)
+		x, err1 := strconv.ParseFloat(num1Str, 64)
+		y, err2 := strconv.ParseFloat(num2Str, 64)
+
+		if err1 != nil && err2 != nil {
+			return c.Status(400).SendString("x and y should be numbers")
+		}
+
+		z := x + y
+
+		return c.JSON(fiber.Map{
+			"x+y": z,
+		})
+	})
+
+	app.Get("/substraction", func(c *fiber.Ctx) error {
+		num1Str := c.Query("x")
+		num2Str := c.Query("y")
+
+		if num1Str == "" || num2Str == "" {
+			return c.Status(400).SendString("Please provide x, y")
+		}
+
+		x, err1 := strconv.ParseFloat(num1Str, 64)
+		y, err2 := strconv.ParseFloat(num2Str, 64)
+
+		if err1 != nil || err2 != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "x and y should be numbers.",
+			})
+		}
+
+		z2 := x - y
+		return c.JSON(fiber.Map{
+			"x-y": z2,
+		})
+	})
+
+	app.Get("/multiplication", func(c *fiber.Ctx) error {
+		num1Str := c.Query("x")
+		num2Str := c.Query("y")
+
+		if num1Str == "" || num2Str == "" {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "Please provide x, y .",
+			})
+		}
+
+		x, err1 := strconv.ParseFloat(num1Str, 64)
+		y, err2 := strconv.ParseFloat(num2Str, 64)
+
+		if err1 != nil && err2 != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "x and y should be numbers.",
+			})
+		}
+
+		z3 := x * y
+		return c.JSON(fiber.Map{
+			"x*y": z3,
+		})
+	})
+
+	app.Get("/division", func(c *fiber.Ctx) error {
+		num1Str := c.Query("x")
+		num2Str := c.Query("y")
+
+		if num1Str == "" || num2Str == "" {
+			return c.Status(400).SendString("Please provide x, y")
+		}
+
+		x, err1 := strconv.ParseFloat(num1Str, 64)
+		y, err2 := strconv.ParseFloat(num2Str, 64)
+
+		if err1 != nil && err2 != nil {
+			return c.Status(400).JSON(fiber.Map{
+				"error": "x and y should be numbers.",
+			})
+		}
+
+		if y == 0 {
+			return c.SendString("y must be non zero")
+		}
+
+		z4 := x / y
+		return c.JSON(fiber.Map{
+			"x/y": z4,
+		})
+
+	})
+	log.Fatal(app.Listen(":3000"))
+}
